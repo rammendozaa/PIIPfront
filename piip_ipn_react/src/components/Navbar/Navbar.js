e7d9c82f512a5abdc443fef841b01f1599b7ce52
Hugo <hugomichelbl@gmail.com>
2022-01-31 23:58:55 -0600

    Add css and finish navbar

--- piip_ipn_react/src/components/Navbar/Navbar.js
+++ piip_ipn_react/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems"
+import { Button } from '../Button';
 import './Navbar.css'
 
 class Navbar extends Component{
@@ -12,7 +13,7 @@ class Navbar extends Component{
     render(){
         return (
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">Reac<i className="fab fa-react"></i></h1>
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
@@ -27,6 +28,7 @@ class Navbar extends Component{
                         )
                     })}
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
         )
     }
