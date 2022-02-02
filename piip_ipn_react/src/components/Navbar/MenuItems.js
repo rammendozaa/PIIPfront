446e1858c19d0472891b37a78d403bf95cd525ce
Hugo <hugomichelbl@gmail.com>
2022-02-01 18:50:25 -0600

    change css

--- piip_ipn_react/src/components/Navbar/MenuItems.js
+++ piip_ipn_react/src/components/Navbar/MenuItems.js
@@ -1,26 +1,11 @@
 export const MenuItems = [
-    {
+    /*{
         title: 'Home',
         url: '#',
         cName: 'nav-links'
-    },
+    },*/
     {
-        title: 'Services',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Products',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Contact Us',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Sign up',
+        title: 'Log In',
         url: '#',
         cName: 'nav-links-mobile'
     },
