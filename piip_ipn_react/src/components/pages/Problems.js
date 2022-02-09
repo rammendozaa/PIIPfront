2c4d18a3b77a3399662bbbfcd80b66bf0bcd4dad
Hugo <hugomichelbl@gmail.com>
2022-02-08 21:18:09 -0600

    Add problems page

--- piip_ipn_react/src/components/pages/Problems.js
+++ piip_ipn_react/src/components/pages/Problems.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Datatable from '../Datatable'
 import '../../App.css'
 
 function Problems() {
+    const [data, setData] = useState([
+        {"title" : "p1", "related_topics" : "dp, greedy", "difficulty" : "easy", "status" : "solved"},
+        {"title" : "p1", "related_topics" : "dp, greedy", "difficulty" : "easy", "status" : "solved"},
+        {"title" : "p1", "related_topics" : "dp, greedy", "difficulty" : "easy", "status" : "solved"},
+        {"title" : "p2", "related_topics" : "dp, greedy", "difficulty" : "easy", "status" : "solved"},
+    ]);
+    const [query, setQuery] = useState("");
+
+    function search(rows){
+        return rows.filter(
+            row => row.title.toLowerCase().indexOf(query.toLowerCase()) > -1
+        )
+    }
+
     return (
         <>
-            <h1>Problems</h1>
+            <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}></input>
+            <Datatable data={search(data)}/>
         </>
     )
 }
