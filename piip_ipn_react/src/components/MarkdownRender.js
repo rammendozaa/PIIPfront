d05dbe251759b9f0d60b6ab30672c3f4476629d6
Hugo <hugomichelbl@gmail.com>
2022-02-25 16:23:53 -0600

    Final version

--- piip_ipn_react/src/components/MarkdownRender.js
+++ piip_ipn_react/src/components/MarkdownRender.js
@@ -2,6 +2,8 @@ import ReactMarkdown from 'react-markdown';
 /*import MathJax from 'react-mathjax';*/
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex'
+import remarkGfm from 'remark-gfm'
+import { BlockMath, InlineMath } from 'react-katex';
 import 'katex/dist/katex.min.css'
 
 function MarkdownRender(props) {
@@ -18,8 +20,12 @@ function MarkdownRender(props) {
       };*/
       const newProps = {
         ...props,
-        remarkPlugins: [remarkMath],
-        rehypePlugins: [rehypeKatex]
+        remarkPlugins: [remarkMath, remarkGfm],
+        rehypePlugins: [rehypeKatex],
+        components: {
+            math: ({value}) => <BlockMath>{value}</BlockMath>,
+            inlineMath: ({value}) => <InlineMath>{value}</InlineMath>
+        }
       };
       /*return (
         <MathJax.Provider input="tex">
