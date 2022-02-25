c2a223be3f702f1366a2148c1bee9de5c2635ec6
Hugo <hugomichelbl@gmail.com>
2022-02-25 15:32:05 -0600

    Add image

--- piip_ipn_react/src/components/pages/Topic.js
+++ piip_ipn_react/src/components/pages/Topic.js
@@ -7,6 +7,7 @@ $$
 s = ut + \\frac{1}{2}at^{2}
 $$
 Calculate the value of $s$ when $u = 10\\frac{m}{s}$ and $a = 2\\frac{m}{s^{2}}$ at $t = 1s$
+![image info](https://www.himgs.com/imagenes/hola/comunes/hola-2017.gif)
 `
   return (
     <>
