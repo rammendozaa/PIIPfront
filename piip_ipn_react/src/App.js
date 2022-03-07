d782204f1f745c50f26a9419d7364a02a27350d2
Hugo <hugomichelbl@gmail.com>
2022-03-06 18:06:45 -0600

    p1

--- piip_ipn_react/src/App.js
+++ piip_ipn_react/src/App.js
@@ -24,15 +24,15 @@ function App() {
         <Navbar validToken={validToken} removeToken={removeToken} role={role} />
         <Routes>
           <Route path='/' exact element={<Home/>}/>
-          <Route path='/problems' element={<PrivateRoute validToken={validToken}><Problems token={token}/></PrivateRoute>}/>
-          <Route path='/topics' exact element={<PrivateRoute validToken={validToken}><Topics/></PrivateRoute>}/>
-          <Route path='/mock-interviews' element={<PrivateRoute validToken={validToken}><MockInterviews/></PrivateRoute>}/>
-          <Route path='/soft-skills' exact element={<PrivateRoute validToken={validToken}><SoftSkills/></PrivateRoute>}/>
-          <Route path='/company-tracking' exact element={<PrivateRoute validToken={validToken}><CompanyTracking/></PrivateRoute>}/>
+          <Route path='/problems' element={<PrivateRoute validToken={validToken} validRoles={["usuario","mentor"]} role={role}><Problems token={token}/></PrivateRoute>}/>
+          <Route path='/topics' exact element={<PrivateRoute validToken={validToken} validRoles={["usuario","mentor"]} role={role}><Topics/></PrivateRoute>}/>
+          <Route path='/mock-interviews' element={<PrivateRoute validToken={validToken} validRoles={["usuario"]} role={role}><MockInterviews/></PrivateRoute>}/>
+          <Route path='/soft-skills' exact element={<PrivateRoute validToken={validToken} validRoles={["usuario","mentor"]} role={role}><SoftSkills/></PrivateRoute>}/>
+          <Route path='/company-tracking' exact element={<PrivateRoute validToken={validToken} validRoles={["usuario"]} role={role}><CompanyTracking/></PrivateRoute>}/>
           <Route path='/log-in' element={<LogIn validToken={validToken} setToken={setToken} setRole={setRole}/>}/>
-          <Route path='/topic' element={<PrivateRoute validToken={validToken}><Topic/></PrivateRoute>} />
-          <Route path='/problem' element={<PrivateRoute validToken={validToken}><Problem/></PrivateRoute>} />
-          <Route path='/my-course' element={<PrivateRoute validToken={validToken}><CourseContent/></PrivateRoute>} />
+          <Route path='/topic' element={<PrivateRoute validToken={validToken} validRoles={["usuario","mentor"]} role={role}><Topic/></PrivateRoute>} />
+          <Route path='/problem' element={<PrivateRoute validToken={validToken} validRoles={["usuario","mentor"]} role={role}><Problem/></PrivateRoute>} />
+          <Route path='/my-course' element={<PrivateRoute validToken={validToken} validRoles={["usuario"]} role={role}><CourseContent/></PrivateRoute>} />
           <Route path='*' element={<NotFound/>} />
         </Routes>
       </Router>
--- piip_ipn_react/src/components/PrivateRoute.js
+++ piip_ipn_react/src/components/PrivateRoute.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
-function PrivateRoute({children, validToken}) {
+function PrivateRoute({children, validToken, validRoles, role}) {
     let location = useLocation();
     if(!validToken()){
         return <Navigate to="/log-in" state={{ from: location }}/>
+    }
+    if(validRoles.includes(role) === false){
+        return <Navigate to="/" state={{ from: location }}/>
     }
     return children
 }
