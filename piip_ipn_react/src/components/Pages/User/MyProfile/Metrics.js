53da33e7a014d93b8c0f82272bb6e92303e5bf1a
Hugo <hugomichelbl@gmail.com>
2022-04-30 17:52:12 -0500

    Add css

--- piip_ipn_react/src/components/Pages/User/MyProfile/Metrics.js
+++ piip_ipn_react/src/components/Pages/User/MyProfile/Metrics.js
@@ -7,8 +7,17 @@ function Metrics({user}) {
     /*
         UserData.map((data) => data.field)
     */
-    const [userData, setUserData] = useState({
+    const [problemsSolved, setProblemsSolved] = useState({
         labels: ["April 20","April 21","April 22","April 23","April 24"],
+        datasets: [{
+            label: "Problems solved in last month",
+            data: [1,2,3,4,5],
+            borderColor: "black",
+            borderWidth: 2
+        }]
+    })
+    const [cntByCategory, setCntByCategory] = useState({
+        labels: ["Dp","Greedy","Brute Force","Graphs","Ad-Hoc"],
         datasets: [{
             label: "Problems solved in last month",
             data: [1,2,3,4,5],
@@ -18,22 +27,48 @@ function Metrics({user}) {
     })
     return (
         <div className="metrics-container">
-            <div className="topics-problems">
-                <h1>Here</h1>
+            <div className="categories">
+                <h3>Vista General</h3>
+                <div className="categories-container">    
+                    <div className="category">
+                        <div className="title">
+                            <h2>Topics</h2>
+                        </div>
+                        <div className="content">
+                            <p>5 topics studied</p>
+                        </div>                        
+                    </div>
+                    <div className="category">
+                        <div className="title">
+                            <h2>Problems</h2>
+                        </div>
+                        <div className="content">
+                            <p>83 problems solved</p>
+                        </div>                        
+                    </div>
+                    <div className="category">
+                        <div className="title">
+                            <h2>Interviews</h2>
+                        </div>
+                        <div className="content">
+                            <p>5 interviews attended</p>
+                        </div>                        
+                    </div>
+                </div>
             </div>
             <div className="charts">
                 <div className="top">
                     <div className="metrics-barchart">
-                        <BarChart data={userData}/>
+                        <BarChart data={problemsSolved}/>
                     </div>
                 </div>
                 <div className="bottom">
                     <div className="bottom-container">
                         <div className="metrics-piechart">
-                            <PieChart data={userData}/>
+                            <PieChart data={cntByCategory}/>
                         </div>
                         <div className="metrics-piechart">
-                            <PieChart data={userData}/>
+                            <PieChart data={cntByCategory}/>
                         </div>
                     </div>
                 </div>
