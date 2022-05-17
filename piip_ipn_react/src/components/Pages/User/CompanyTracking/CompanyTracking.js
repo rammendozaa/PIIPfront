a6ae60dda1a16c9e9b8154a22890206842bc69ea
Alvaro <ram.mendoza.a.98@outlook.com>
2022-05-16 22:09:07 -0500

    Modify icons and add company tracking

--- piip_ipn_react/src/components/Pages/User/CompanyTracking/CompanyTracking.js
+++ piip_ipn_react/src/components/Pages/User/CompanyTracking/CompanyTracking.js
@@ -1,3 +1,6 @@
+import { IconContext } from 'react-icons';
+import { FiCheck, FiPlus, FiSlash, FiEdit3 } from 'react-icons/fi';
+import { TiDelete } from 'react-icons/ti'
 import React, { useState } from 'react'
 import { useEffect } from 'react/cjs/react.development'
 import Datatable from '../../../Datatable/Datatable'
@@ -7,10 +10,50 @@ const baseURL = "http://127.0.0.1:5000"
 
 
 function CompanyTracking({userData}) {
-    const user_id = userData.user_id
-    const [companies, setCompanies] = useState([])
-
-
+    const [newActivityIndex, setNewActivityIndex] = useState(-1)
+    const [newActivitySectionId, setNewActivitySectionId] = useState(-1)
+    const user_id = userData.user_id;
+    const [description, setDescription] = useState("")
+    const [clicked, setClicked] = useState(-1);
+    const [companies, setCompanies] = useState([
+        {
+            "applicationURL": "https://www.metacareers.com/",
+            "id": 1,
+            "userId": 1,
+            "statusId": 1,
+            "interviewDate": null,
+            "companyId": 1,
+            "trackingLinks": [
+                {
+                    "description": "Meta openings",
+                    "id": 1,
+                    "url": "https://www.metacareers.com/jobs",
+                    "companyTrackingId": 1,
+                    "is_active": true
+                },
+                {
+                    "description": "hla",
+                    "id": 2,
+                    "url": "here",
+                    "companyTrackingId": 1,
+                    "is_active": true
+                }
+            ],
+            "companyName": "Facebook"
+        }])
+        const toggle = index => {
+            if (clicked === index) {
+                //if clicked question is already active, then close it 2
+                return setClicked(-1);
+            }   
+            setClicked(index);
+        };
+    
+    const prepareNewSection = (index, templateSectionId) => {
+        setNewActivityIndex(index)
+        setNewActivitySectionId(templateSectionId)
+    }
+    
     const getCompanies = async () => {
         fetch(baseURL + `/user/${user_id}/tracking`, {
             method: "GET",
@@ -80,7 +123,7 @@ function CompanyTracking({userData}) {
     }
 
 
-    const deleteCompanyTracking = async (company_tracking_id) => {
+    const deleteCompanyTracking = async (indexCompany, company_tracking_id) => {
         const response = await fetch(baseURL + `/user/tracking/${company_tracking_id}`, {
             method: "DELETE",
         })
@@ -88,7 +131,7 @@ function CompanyTracking({userData}) {
         getCompanies();
     }
 
-    const deleteCompanyTrackingLink = async(company_tracking_link_id) => {
+    const deleteCompanyTrackingLink = async(indexLink, company_tracking_link_id) => {
         const response = await fetch(baseURL + `/user/tracking/link/${company_tracking_link_id}`, {
             method: "DELETE",
         })
@@ -96,44 +139,6 @@ function CompanyTracking({userData}) {
         getCompanies();
     }
 
-    const [data, setData] = useState([
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p1", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-        {"Company" : "p2", "Link" : "www.google.com", "next-interview" : "calendar", "status" : "solved"},
-    ]);
     const [query, setQuery] = useState("");
 
     function search(rows){
@@ -141,25 +146,74 @@ function CompanyTracking({userData}) {
             row => row.companyName.toLowerCase().indexOf(query.toLowerCase()) > -1
         )
     }
-/*
-            <div className='company-container'>
-                <div className='search_wrap'>
-                    <div className='search_box'>
-                        <div className='btn btn-common'>
-                            <i className='fas fa-search'></i> 
+
+    return(
+        <>
+            <div className='course-content-container'>
+                {companies !== undefined && (<div className='course'> 
+                    <h1 className='course-title'>{"Here you can keep track of the companies that you're interested in applying to."}</h1>
+                    <IconContext.Provider value={{ color: 'red', size: '25px' }}>
+                        <div className='AccordionSection'>
+                            <div className='Container'>
+                                {companies.map((company, indexCompany) => {
+                                    return (
+                                    <>
+                                        <div className='Wrap' key={indexCompany}>
+                                            <h1 onClick={() => toggle(indexCompany)}>{company.companyName}</h1>
+                                            <span><TiDelete onClick={() => deleteCompanyTracking(indexCompany, company.id)}/></span>
+                                        </div>
+                                        <div className='activities-container'>
+                                        {
+                                            clicked === indexCompany 
+                                                ? 
+                                                (
+                                                    company.trackingLinks.map((link,indexLink) => {
+                                                        return (
+                                                            <>
+                                                                <div className='Dropdown' key={indexLink}>
+                                                                    <p><b>{link.description}</b>: {link.url}</p>
+                                                                    <span><TiDelete onClick={() => deleteCompanyTrackingLink(indexLink, link.id)}/></span>
+                                                                </div>                                                                
+                                                            </>
+                                                        )
+                                                    })
+                                                ) 
+                                                : null
+                                        }
+                                        {
+                                            clicked === indexCompany
+                                            ?
+                                            (
+                                                <div className='AddNewActivity'>
+                                                    <input type='text' placeholder='Section Name' className='input' value={description} onChange={(e) => setDescription(e.target.value)}/>
+                                                    <span>{<FiPlus onClick={() => addCompanyTrackingLink()}/>}</span>
+                                                    <span>{<FiPlus onClick={() => prepareNewSection(indexCompany, company.id) }/>}</span>
+                                                </div>
+                                            )
+                                            :null
+                                        }
+                                        </div>
+                                    </>
+                                    );
+                                })}
+                                <div className='AddNewSection'>
+                                    <h2>Add a new company to track: </h2>
+                                    <span>{<FiPlus />}</span>
+                                </div>
+                            </div>
                         </div>
-                        <input type="text" className='input' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search ...'></input>
-                    </div>
-                </div>
-                <Datatable data={search(data)}/>
+                    </IconContext.Provider>
+                </div>)}
             </div>
+        </>
+    )
 
-*/
-
+    /*
     return (
         <>
         </>
-    )
+        )
+        */
 }
 
 export default CompanyTracking
