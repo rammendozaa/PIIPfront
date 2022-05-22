011fc804693b650b59db0a40dfd2f4bff2e235b5
Hugo <hugomichelbl@gmail.com>
2022-05-22 12:29:56 -0500

    Change color p1

--- piip_ipn_react/src/components/Pages/LogIn/FormLoginContainer.js
+++ piip_ipn_react/src/components/Pages/LogIn/FormLoginContainer.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
-import FormSuccess from './FormSuccess';
+import { Navigate } from 'react-router-dom';
 import FormLogin from './FormLogIn'
 import './FormLoginContainer.css';
 
 export const FormLoginContainer = ({validUserData, setUserData}) => {
+    const [close, setClose] = useState(false)
+    if(close === true){
+        return (
+            <Navigate to="/"/>
+        )
+    }
     return (
         <>
             <div className='form-container'>
-                <span className='close-btn'>x</span>
+                <span className='close-btn' onClick={() => setClose(true)}>x</span>
                 <div className='form-content-left'>
                     <img src='images/home.png' alt='happy' className='form-img'/>
                 </div>
