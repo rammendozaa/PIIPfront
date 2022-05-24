ec8a707fb00aa3c3baa089bb4a51e2bedad020e0
Alvaro <ram.mendoza.a.98@outlook.com>
2022-05-24 09:33:36 -0500

    fixes interview and tracking

--- piip_ipn_react/src/components/Pages/Admin/MyInterviews/MyInterviews.js
+++ piip_ipn_react/src/components/Pages/Admin/MyInterviews/MyInterviews.js
@@ -5,6 +5,8 @@ import { ActivityInfo } from '../../../../externalClasses';
 import { setUserActivityInfo } from '../../../../state/reducers/activity';
 import { useNavigate } from "react-router-dom";
 import "./MyInterviews.css"
+import { FiCheck, FiX } from 'react-icons/fi';
+import { IconContext } from 'react-icons';
 
 const baseURL = "http://127.0.0.1:5000"
 
@@ -40,15 +42,32 @@ function MyInterviews({userData}) {
             <div className='my-interviews-container'>
                 <div className="my-interviews">
                     {interviews.length > 0 && (<>
-                        <h1 className='interview-title'>Here are you're pending interviews:</h1>
+                        <h1 className='interview-title'>Here are your pending interviews. Click to view the details:</h1>
                             {interviews.map((interview, indexInterview) => {
                             return (
                                     <>
-                        <div className='Container'>
-                                    <div>
-                                        <h1 onClick={() => handleClick(interview)}>{interview.user.first_name}</h1>
+                                    <div className='Container' onClick={() => handleClick(interview)}>
+                                        <span>
+                                            <h1>{interview.user.first_name}</h1>
+                                        </span>
+                                        <span>
+                                            {interview.chosenDate && <h2>You have an interview on {interview.chosenDate}</h2>}
+                                            {!interview.chosenDate && <h2>No interview date chosen. Click to add one</h2>}
+                                        </span>
+                                        <span>
+                                            <h2>Confirmed?</h2>
+                                                <IconContext.Provider
+                                                value={{ color: "red", size: "25px" }}
+                                                >
+                                            {interview.isConfirmed && 
+                                                    <FiCheck/>
+                                            }
+                                            {!interview.isConfirmed && 
+                                                    <FiX/>
+                                            }
+                                                </IconContext.Provider>
+                                        </span>
                                     </div>
-                        </div>
                                     </>
                                 )
                             })}
